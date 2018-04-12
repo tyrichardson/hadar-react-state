db0a18cb37b0d2b1055aa7bd400e4325df1b0552
Ty Richadson <tyLrichardson63@gmail.com>
2018-04-11 20:09:47 -0500

    completed creation of button that updates DOM with user input

--- src/App.js
+++ src/App.js
@@ -10,48 +10,104 @@ class App extends Component {
       name: 'Ty',
       city:'Minneapolis',
       zip: 55406
-      }
-    }
+      },
+        userInputs: {
+          name: '',
+          city: '',
+          zip: ''
+        }
+    };
 
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleCityChange = this.handleCityChange.bind(this);
-    this.handleZipChange = this.handleZipChange.bind(this);
+    // this.handleNameChange = this.handleNameChange.bind(this);
+    // this.handleCityChange = this.handleCityChange.bind(this);
+    // this.handleZipChange = this.handleZipChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    //this.handleChangeFor = this.handleChangeFor.bind(this); not needed because arrow function used
   }
 
-handleNameChange(event) {
-  console.log(event.target.value);
-  //this.state.user = event.target.value is NOT the way to go -- no mutating!
-  this.setState({
-    user: {
-      ...this.state.user,
-    name: event.target.value
+// handleNameChange(event) {
+//   console.log(event.target.value);
+//   //this.state.user = event.target.value is NOT the way to go -- no mutating!
+//   this.setState({
+//     user: {
+//       ...this.state.user,
+//     name: event.target.value
+//     }
+//     //the spread operator (...) says give me everything already in there, only update the thing(s) I specifically name
+//   });
+// }
+
+// handleCityChange(event) {
+//   console.log(event.target.value);
+//   //this.state.user = event.target.value is NOT the way to go -- no mutating!
+//   this.setState({
+//     user: {
+//       ...this.state.user,
+//     city: event.target.value
+//   }
+//  })
+// }
+
+  // handleZipChange(event) {
+  //   console.log(event.target.value);
+  //   //this.state.user = event.target.value is NOT the way to go -- no mutating!
+  //   this.setState({
+  //     user: {
+  //       ...this.state.user,
+  //       zip: event.target.value
+  //     }
+  //   })
+  // }
+
+  handleChangeFor = (propertyName) => {
+    return (event) => {
+      this.setState({
+        userInputs: {
+          ...this.state.userInputs,
+          [propertyName]: event.target.value,
+        }
+      });
     }
-  });
-}
-
-handleCityChange(event) {
-  console.log(event.target.value);
-  //this.state.user = event.target.value is NOT the way to go -- no mutating!
-  this.setState({
-    user: {
-      ...this.state.user,
-    city: event.target.value
   }
- })
-}
 
-  handleZipChange(event)
-  {
-    console.log(event.target.value);
-    //this.state.user = event.target.value is NOT the way to go -- no mutating!
+  handleSubmit(event) {
+    event.preventDefault();
     this.setState({
-      user: {
-        ...this.state.user,
-        zip: event.target.value
+      user: { ...this.state.userInputs},
+      userInputs: {
+        name: '',
+        city: '',
+        zip: ''
       }
-    })
+    });
   }
 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.userInputs.name} placeholder='name' onChange={this.handleChangeFor("name")} />
+          <p>Your name: {this.state.user.name}</p>
+          <input value={this.state.userInputs.city} placeholder='city' onChange={this.handleChangeFor("city")} />
+          <p>You're from: {this.state.user.city}</p>
+          <input value={this.state.userInputs.zip} placeholder='zip code' onChange={this.handleChangeFor("zip")} />
+          <p>Your mail gets sorted to: {this.state.user.zip}</p>
+          <input type='submit' value='Submit' />
+        </form>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//example of updating values in an object
 // const bowler = {
 //   name: 'Ty',
 //   lastBowlingScore: 150
@@ -69,33 +125,3 @@ handleCityChange(event) {
 // }
 
 //any time STATE changes, RENDER runs -- refreshes DOM with every change
-  render() {
-    return (
-      <div className="App">
-      
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
-        <div>
-          <input onChange={this.handleNameChange} />
-          <br />
-          You typed: {this.state.user.name}
-          <br />
-          <input onChange = {this.handleCityChange} />
-          <br />
-          You typed: {this.state.user.city}
-          <br />
-          <input onChange={this.handleZipChange} />
-          <br />
-          You typed: {this.state.user.zip}
-
-        </div>
-
-      </div>
-    );
-  }
-}
-
-export default App;
